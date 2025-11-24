8c7319cf80d29cca7a9111d2281ba26051b651db
Mike <mcarans@yahoo.co.uk>
2025-11-25 11:57:10 +1300

    HDXDSYS-2518 Modernise and fix dataset archiver (#4)
    * Modernise archiver
    
    * Fix styling

--- run.py
+++ run.py
@@ -1,20 +1,4 @@
-from os.path import join, expanduser
+import runpy
 
-from hdx.api.configuration import Configuration
-from hdx.facades.simple import facade
-from hdx.utilities.dateparse import now_utc
-
-from archive_datasets import archive
-
-
-def main():
-    archive(Configuration.read(), now_utc())
-
-
-if __name__ == "__main__":
-    facade(
-        main,
-        user_agent_config_yaml=join(expanduser("~"), ".useragents.yml"),
-        user_agent_lookup="hdx-dataset-archiver",
-        project_config_yaml=join("config", "project_configuration.yml"),
-    )
+# Execute a module by its full module name
+runpy.run_module("hdx.dataset.archiver", run_name="__main__")
---
+++ src/hdx/dataset/archiver/__main__.py
@@ -0,0 +1,19 @@
+from os.path import expanduser, join
+
+from hdx.api.configuration import Configuration
+from hdx.dataset.archiver.archive_datasets import archive
+from hdx.facades.simple import facade
+from hdx.utilities.dateparse import now_utc
+
+
+def main():
+    archive(Configuration.read(), now_utc())
+
+
+if __name__ == "__main__":
+    facade(
+        main,
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
+        user_agent_lookup="hdx-dataset-archiver",
+        project_config_yaml=join("config", "project_configuration.yaml"),
+    )
